718a9f58a13b63783fa92b58c7d96cd7d394a959
caohuaqing <763072963@qq.com>
2020-03-28 18:51:02 +0800

    滚动时上方搜索栏被下面挡住了，z-index设置可能由问题

--- src/pages/home/components/SeriesView/index.tsx
+++ src/pages/home/components/SeriesView/index.tsx
@@ -26,6 +26,22 @@ export default class SeriesView extends Component {
           <Image className='series-image' src={item}></Image>
           <View className='series-text'>Air Jordan</View>
         </View>
+        <View className='series-cell'>
+          <Image className='series-image' src={item}></Image>
+          <View className='series-text'>Air Jordan</View>
+        </View>
+        <View className='series-cell'>
+          <Image className='series-image' src={item}></Image>
+          <View className='series-text'>Air Jordan</View>
+        </View>
+        <View className='series-cell'>
+          <Image className='series-image' src={item}></Image>
+          <View className='series-text'>Air Jordan</View>
+        </View>
+        <View className='series-cell'>
+          <Image className='series-image' src={item}></Image>
+          <View className='series-text'>Air Jordan</View>
+        </View>
       </View>
     )
   }
