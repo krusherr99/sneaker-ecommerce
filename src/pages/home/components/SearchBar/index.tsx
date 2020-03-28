3f1089c99ef3e861c458a6654bb1f6b7eef09fc0
caohuaqing <763072963@qq.com>
2020-03-28 15:37:52 +0800

    顶部搜索栏

--- src/pages/home/components/SearchBar/index.tsx
+++ src/pages/home/components/SearchBar/index.tsx
@@ -1,11 +1,21 @@
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View } from '@tarojs/components'
+import { View, Image, Icon } from '@tarojs/components'
+
+import './index.less'
+
+import all from '../../../../static/images/all@3x.png'
 
 export default class Index extends Component {
 
   render () {
     return (
-      <View>我是SearchBar</View>
+      <View className='search-view'>
+        <View className='search-input'>
+          <Icon type='search' size='14'></Icon>
+          <View className='search-title'>搜索单品</View>
+        </View>
+        <Image className='index-series' src={all}></Image>
+      </View>
     )
   }
 }
