828f3d172fb978afd855e38d73921fc0bb4fa132
caohuaqing <763072963@qq.com>
2020-03-28 21:31:11 +0800

    flex如何设置不挤压Item

--- src/pages/home/components/HotList/index.tsx
+++ src/pages/home/components/HotList/index.tsx
@@ -21,6 +21,19 @@ export default class HotList extends Component {
             <View className='sold-number'>5957人付款</View>
           </View>
         </View>
+
+        <View className='product'>
+          <Image className='product-image' src={item}></Image>
+          <View className='product-title'>Air Jordan 11 康扣</View>
+          <View className='price-info'>
+            <View className='unit-price-view'>
+              <View className='unit'>￥</View>
+              <View className='price'>2279</View>
+            </View>
+            <View className='sold-number'>5957人付款</View>
+          </View>
+        </View>
+
         <View className='product'>
           <Image className='product-image' src={item}></Image>
           <View className='product-title'>Air Jordan 11 康扣</View>
