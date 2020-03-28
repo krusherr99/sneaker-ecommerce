3974f3daa920fbeee7d91d546ef0446f0264eb78
caohuaqing <763072963@qq.com>
2020-03-28 21:26:37 +0800

    为啥这里设置了boarder-right没有用

--- src/pages/home/components/HotList/index.tsx
+++ src/pages/home/components/HotList/index.tsx
@@ -7,11 +7,30 @@ import item from './item.jpg'
 
 export default class HotList extends Component {
 
-  render () {
+  render() {
     return (
       <View className='hot-list'>
-        <View className='item'>
+        <View className='product'>
           <Image className='product-image' src={item}></Image>
+          <View className='product-title'>Air Jordan 11 康扣</View>
+          <View className='price-info'>
+            <View className='unit-price-view'>
+              <View className='unit'>￥</View>
+              <View className='price'>2279</View>
+            </View>
+            <View className='sold-number'>5957人付款</View>
+          </View>
+        </View>
+        <View className='product'>
+          <Image className='product-image' src={item}></Image>
+          <View className='product-title'>Air Jordan 11 康扣</View>
+          <View className='price-info'>
+            <View className='unit-price-view'>
+              <View className='unit'>￥</View>
+              <View className='price'>2279</View>
+            </View>
+            <View className='sold-number'>5957人付款</View>
+          </View>
         </View>
       </View>
     )
