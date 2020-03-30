9c14df7025568853129d3d15d5eb998784f6837e
caohuaqing <763072963@qq.com>
2020-03-30 16:41:52 +0800

    完成popup静态静态布局

--- src/pages/product/ProductSearchResult/index.tsx
+++ src/pages/product/ProductSearchResult/index.tsx
@@ -7,6 +7,7 @@ import SearchFilters from './components/SearchFilters'
 import HotList from '../../home/components/HotList/index';
 
 import './index.less'
+import classNames from 'classnames';
 
 const sizeList = ["35.5", "36", "36.5", "37", "37.5", "38", "38.5", "39", "39.5", "40", "40.5", "41", "41.5", "42", "42.5", "43", "43.5", "44", "44.5", "45", "45.5", "46", "46.5", "47", "47.5", "48", "48.5", "全部"]
 
@@ -20,9 +21,10 @@ export default class ProductSearchResult extends Component {
           <View className='size-pop-view'>
             {
               sizeList.map((item: string) => {
+                const all = item === '全部'
                 return (
                   <View className='size-flex-view'>
-                    <View className='size-item'>{item}</View>
+                    <View className={classNames('size-item', { 'size-all': all })}>{item}</View>
                   </View>
                 )
               })
