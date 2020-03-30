7062cfb93736481dff4d7d117dab69a1feb035ce
caohuaqing <763072963@qq.com>
2020-03-30 17:42:44 +0800

    有报错信息Cannot read property 'selectSales' of undefined，猜测可能是 defaultProps没有设置

--- src/pages/product/ProductSearchResult/components/SearchFilters/index.tsx
+++ src/pages/product/ProductSearchResult/components/SearchFilters/index.tsx
@@ -7,20 +7,33 @@ import './index.less'
 
 import price_arrow from './price_arrow.png'
 import size_arrow from './size_arrow.png'
+import { ProductSearchResultState } from '../..';
 
-export default class SearchBox extends Component {
+interface SearchBoxProps extends ProductSearchResultState { 
+  toggleSelectSales: (selectSales: boolean) => void
+}
+
+export default class SearchBox extends Component<SearchBoxProps> {
+
+  handleClick = () => {
+    const { toggleSelectSales } = this.props
+    console.log(this.props.selectMap.selectSales);
+    toggleSelectSales(!this.props.selectMap.selectSales)
+  }
+
   render() {
+    let { selectSales, selectNew } = this.props.selectMap
     return (
       <View className='filters-info'>
         <View className='filter-border-view'>
           <View className='filter-view'>
-            <View className='select-sales-view'>销量</View>
+            <View className={classNames('sales-view', { 'select': selectSales })} onClick={this.handleClick}>销量</View>
             <View className='price-wrap'>
               <View className='price-view'>价格</View>
               <Image className='price-arrow' src={price_arrow}></Image>
             </View>
-            <View className='new-view'>新品</View>
-            <View className='size-wrap'>
+            <View className={classNames('new-view', { 'select': selectNew })}>新品</View>
+            <View className='size-wrap' onClick={this.handleClick}>
               <View className='size-view'>尺码</View>
               <Image className='size-arrow' src={size_arrow}></Image>
             </View>
--- src/pages/product/ProductSearchResult/index.tsx
+++ src/pages/product/ProductSearchResult/index.tsx
@@ -11,12 +11,40 @@ import classNames from 'classnames';
 
 const sizeList = ["35.5", "36", "36.5", "37", "37.5", "38", "38.5", "39", "39.5", "40", "40.5", "41", "41.5", "42", "42.5", "43", "43.5", "44", "44.5", "45", "45.5", "46", "46.5", "47", "47.5", "48", "48.5", "全部"]
 
-export default class ProductSearchResult extends Component {
+interface ProductSearchResultProps { }
+export interface ProductSearchResultState {
+  selectMap: {
+    selectSales?: boolean;
+    selectPrice?: boolean;
+    selectNew?: boolean;
+    selectSize?: boolean;
+  }
+}
+
+export default class ProductSearchResult extends Component<ProductSearchResultProps, ProductSearchResultState> {
+
+  state: ProductSearchResultState = {
+    selectMap: {
+      selectSales: false,
+      selectPrice: false,
+      selectNew: false,
+      selectSize: false
+    }
+  }
+
+  toggleSelectSales = (selectSales: boolean) => {
+    this.setState({ selectMap: {selectSales: selectSales}  })
+  }
+
+
+
   render() {
+    let { selectMap } = this.state
     return (
+      
       <View className='max-height'>
         <SearchBox />
-        <SearchFilters />
+        <SearchFilters selectMap={selectMap} toggleSelectSales={this.toggleSelectSales} />
         <View className='bg_screen'>
           <View className='size-pop-view'>
             {
